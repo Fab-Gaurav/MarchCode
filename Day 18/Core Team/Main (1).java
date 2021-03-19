b31b75765ebcf27fc5531a550cab5043db45d2ba
Shubhangi Gupta <shubhigupta991@gmail.com>
2021-03-20 01:03:57 +0530

    Day 17 contributions and Day 18 question

---
+++ Day 18/Core Team/Main (1).java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DayEighteen {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of rows: ");
+		int row = sc.nextInt();
+		System.out.println("Enter the number of columns: ");
+		int col = sc.nextInt();
+		
+		if(row = col) {
+			System.out.println("Number of rows and colums must be same in a matrix!");
+		}else {
+			int arr[][] = new int[row][col];
+			
+			for(int i = 0 ; i<row; i++) {
+				for(int j = 0 ; j<col ; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			
+			for(int i = 0 ; i<row; i++) {
+				for(int j = 0; j<col; j++) {
+					if(i<j) {
+						System.out.print(arr[i][j]+" ");
+					}else
+						System.out.print("0 ")
+				}System.out.println()
+			}
+			
+		}
+		
+	}
+}
---
+++ Day 18/Core Team/main (1).py
@@ -0,0 +1,15 @@
+def upper_triangle_matrix(mat,row,col):
+	for i in range(row):
+		for j in range(col):
+			if i>j:
+				print("0" , end = " ")
+			else:
+				print(mat[i][j] , end =" ")
+		print(" ")
+
+mat = [1,2,3],
+	  [4,5,6],
+	  [7,8,9]
+row = 3
+col = 3
+upper_triangle_matrix(mat,row,col)
