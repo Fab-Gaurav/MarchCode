ba7ac7cce46b466292735e6003a9d6a0205f071e
ChetasShree <cm7310@srmist.edu.in>
2021-03-20 07:58:38 +0530

    Day 17 error Resolved #230

--- Day 17/Core Team/python.py
+++ Day 17/Core Team/python.py
@@ -1,12 +1,12 @@
 def fibonacci_series(n):
 	a = 0
 	b = 1
-	if a =< 0:
+	if a < 0:
 		return;
 	print(a , end = " ")
-	for i in range(1,n+1):
+	for i in range(1,n):
 		print(b,end =" ")
-		c = a-b
+		c = a+b
 		a = b
 		b = c
 n = int(input("Enter the value for n : "))
