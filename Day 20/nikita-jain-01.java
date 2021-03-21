a62a85e0c39862ab08b1801ef8cabaee2c17fc3b
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-21 11:55:15 +0530

    Day 20 contribution

---
+++ Day 20/nikita-jain-01.java
@@ -0,0 +1,27 @@
+/*
+Input -> 5
+
+Output -> 1
+          2 2
+          3 3 3
+          4 4 4 4
+          5 5 5 5 5
+*/
+
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+                System.out.print(i+" ");
+            System.out.println();
+        }
+    }
+}
