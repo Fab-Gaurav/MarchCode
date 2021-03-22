ca7cc698514607a887f6a319c165b6309f01c57e
Jatin Saini <sainijat7854@gmail.com>
2021-03-22 13:19:18 +0530

    Day 17 java error resolved #231

--- Day 17/Core Team/java.java
+++ Day 17/Core Team/java.java
@@ -6,15 +6,16 @@ class DaySeventeen{
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
         int a=0,b=1,i,sum=0;
-        
-        if(n==1)
+        if(n<=0)
+            return;
+        else if(n==1)
             System.out.println(a);
         else if(n==2)
             System.out.println(a+" "+b);
         else{
             System.out.print(a+" "+b+" ");
             
-            for(i=2; i<=n; i++){
+            for(i=3; i<=n; i++){
                 sum=a+b;
                 a=b;
                 b=sum;
