46f6c6e8a09a8876ee3fc11a3eba4af803d096dc
Tushar Wadhwa <66528495+twadhwa@users.noreply.github.com>
2021-03-22 05:35:31 +0530

    Create C.c

---
+++ Day 20/Core Team/C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n,i,j;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<i+1;j++)
+        {
+            printf("%d ", i);
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
