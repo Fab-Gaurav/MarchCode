b029f9e88eea13a67f88959a71950be4a9e93816
ChetasShree <75165587+ChetasShree@users.noreply.github.com>
2021-03-23 12:31:04 +0530

    Day 22 in Python

---
+++ day22.py
@@ -0,0 +1,6 @@
+# "To Find the maximum and minimum number in an array"
+n = list(map(int,input().split()))
+max = max(n)
+min = min(n)
+print(f"The minimum no. in the array is {min}")
+print(f"The maximum no. in the array is {max}")
