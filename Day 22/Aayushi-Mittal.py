5612f93cfd397da67ab060d6d667372cd0e8b5ca
Aayushi Mittal <60853067+Aayushi-Mittal@users.noreply.github.com>
2021-03-24 22:54:36 +0530

    Did few corrections

--- Day 22/Aayushi-Mittal.py
+++ Day 22/Aayushi-Mittal.py
@@ -3,12 +3,12 @@
 n=int(input()) 
 arr=[] 
 maximum=-100000
-minimum=100000;
+minimum=100000
 for i in range(0,n):
     arr.append(int(input()))
     if maximum<arr[i]:
         maximum = arr[i]
     if minimum>arr[i]:
         minimum = arr[i]
-print("> Maximum : ",maximum);
-print("> Minimum : ",minimum);
+print("> Maximum : ",maximum)
+print("> Minimum : ",minimum)
