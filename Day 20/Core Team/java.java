36cd0964434749f2b37a68f89dc9d0b704b8c986
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-27 17:39:51 +0530

    Error in Day 20 java #283

--- Day 20/Core Team/java.java
+++ Day 20/Core Team/java.java
@@ -6,8 +6,8 @@ public class DayTwenty {
 		System.out.println("Enter the number of rows: ");
 		int n = sc.nextInt();
 		
-		for(int i = 0 ; i <= n ; i++) {
-			for(int j = 0; j < i ; j++) {
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = 1; j <= i ; j++) {
 				System.out.print(i+" ");
 			}System.out.println();
 		}
