e4599d13b2070ac1885737ffdb109c3cd908756c
Chirag Mathur <mchirag2002@gmail.com>
2021-03-28 01:44:40 +0530

    Changes done in C by Chirag Mathur

--- Day 18/Core Team/main_1.c
+++ Day 18/Core Team/main_1.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-int main() {
+#include <stdio.h>
+int main() 
+{
    int rows, cols, r, c, matrix[10][10];
    printf("Please enter the number of rows for the matrix: ");
    scanf("%d", &rows);
@@ -13,22 +14,22 @@ int main() {
          scanf("%d", &matrix[r][c]);
       }
    }
-   printf("\n The Lower Triangular Matrix is: ");
+   printf("\n\n The Upper Triangular Matrix is: ");
    for(r = 0; r < rows; r++)
    {
       printf("\n");
       for(c = 0; c < cols; c++)
       {
-         if(r >= c)
-         {
-            printf("%d\t ", matrix[r][c]);
-         }
-         else
-         {
+         if(r > c){
             printf("0");
             printf("\t ");
          }
+         else{
+            printf("%d\t ", matrix[r][c]);
+
+         }
       }
    }
+   
    return 0;
 }
