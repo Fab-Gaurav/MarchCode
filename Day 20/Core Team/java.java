44a52549e8bdf05da434de7f8c804e45f004145c
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-27 17:50:37 +0530

    Error in Day 20 java #283

--- Day 20/Core Team/java.java
+++ Day 20/Core Team/java.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class DayTwenty {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of rows: ");
+		System.out.print("Enter the number of rows: ");
 		int n = sc.nextInt();
-		
+		System.out.println(n);
 		for(int i = 1 ; i <= n ; i++) {
 			for(int j = 1; j <= i ; j++) {
 				System.out.print(i+" ");
