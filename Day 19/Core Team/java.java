c94c18ccabbd4b57b0485d74dbf1e78fd422f292
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-28 15:37:00 +0530

    Error in Day 19 java #279

--- Day 19/Core Team/java.java
+++ Day 19/Core Team/java.java
@@ -4,33 +4,34 @@ public class DayEighteen {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter the number of rows: ");
+		System.out.print("Enter the number of rows: ");
 		int row = sc.nextInt();
-		System.out.println("Enter the number of columns: ");
+        System.out.println(row);
+		System.out.print("Enter the number of columns: ");
 		int col = sc.nextInt();
+        System.out.println(col);
 		
 		if(row != col) {
 			System.out.println("Number of rows and colums must be same in a matrix!");
-		}else {
+		}
+		else{
 			int arr[][] = new int[row][col];
 			
 			for(int i = 0 ; i<row; i++) {
 				for(int j = 0 ; j<col ; j++) {
 					arr[i][j] = sc.nextInt();
 				}
-			}
-			
+			}			
 			
 			for(int i = 0 ; i<row; i++) {
 				for(int j = 0; j<col; j++) {
-					if(i>j) {
+					if(i>=j)
 						System.out.print(arr[i][j]+" ");
-					}else
+					else
 						System.out.print("0 ");
-				}System.out.println();
-			}
-			
+				}
+				System.out.println();
+			}	
 		}
-		
 	}
 }
